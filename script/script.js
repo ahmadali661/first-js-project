754b365008050909da070f0442cc7f772d9860e4
Ahmad Ali Sabir <amd.ali661@gmail.com>
2024-03-17 15:33:09 +0500

    add code tag

--- script/script.js
+++ script/script.js
@@ -21,7 +21,7 @@ document.getElementById("alertNumber").onclick = function () {
 }
 
 document.getElementById("varibleNames").onclick = function () {
-    let html = "<ul><li>A variable name can't contain any spaces.</li><li>A variable name can contain only letter, numbers, dollar signs and underscores.</li><li>Though a variable name can't be any of javascript's keywords. it can contain keywords. For example. userAlert and myNav are legal. </li><li>Capital letters are fine, but be careful. Variable names are case sensitive. A rose is not a Rose. If you assign the string 'Floribundas' to the variable rose, and then ask JavaScript for the value assigned to Rose, you'll come up empty.</li></ul> "
+    let html = "<ul><li>A variable name can't contain any spaces.</li><li>A variable name can contain only letter, numbers, dollar signs and underscores.</li><li>Though a variable name can't be any of javascript's keywords. it can contain keywords. For example. <code>userAlert</code> and <code>myNav</code> are legal. </li><li>Capital letters are fine, but be careful. Variable names are case sensitive. A <code>rose</code> is not a <code>Rose</code>. If you assign the string 'Floribundas' to the variable <code>rose</code>, and then ask JavaScript for the value assigned to<code>Rose</code>, you'll come up empty.</li></ul> "
     document.getElementById("result").innerHTML = html;
 }
 document.getElementById("camelCaseExamples").onclick = function () {
